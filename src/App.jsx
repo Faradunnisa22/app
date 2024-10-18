5e97b4a8a1308818971d0c6e2944923ea1953a0b
Faradunnisa21 <ffaradunnisa@gmail.com>
2024-10-18 14:44:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+
+import './App.css'
+import About from './components/About'
+import Feature from './components/Feature'
+import Feature2 from './components/Feature2'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Services from './components/Services'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+      <Feature/>
+      <Feature2/>
+      <Services/>
+      <About/>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*,{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      
+        colors: {
+          'neutralBlue':'#234FEB',
+          'neutralPurple':'#A361FF',
+          'neutralDarkBlue':'#011DAA',
+
+      }
+    },
+  },
+    plugins: [require('flowbite/plugin')],
+}
+
